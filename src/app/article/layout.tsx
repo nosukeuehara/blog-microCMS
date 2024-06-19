08ca2c46ffc92ea1e1e3d5d756d5eaf06bfe7f0a
DE-Uehara <r.uehara@defide-ix.com>
2024-06-19 11:33:03 +0900

    レイアウト修正

--- src/app/article/layout.tsx
+++ src/app/article/layout.tsx
@@ -8,8 +8,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Navigation />
-      <div className="grid grid-cols-4 gap-4">
-        <div className="col-span-3">
+      <div className="grid grid-cols-5 gap-4">
+        <div className="col-span-4">
           <main>{children}</main>
         </div>
         <div className="col-span-1">
