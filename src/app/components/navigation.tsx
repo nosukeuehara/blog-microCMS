abd1172387dccf0d76dcc58fce422a16de49a15d
DE-Uehara <r.uehara@defide-ix.com>
2024-06-19 09:38:44 +0900

    諸々修正

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,33 +1,12 @@
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">
-            home
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            about
-          </Link>
-        </li>
-        <li>
-          <Link href="/services">
-            art
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-            contact
-          </Link>
-        </li>
-      </ul>
+    <nav className=" flex justify-start">
+      <Link href="/article">home</Link>
     </nav>
   );
-}
+};
 
-export default Navigation
+export default Navigation;
