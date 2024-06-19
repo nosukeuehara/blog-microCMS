270c1a1006bb2e39ccecba3de38e39e96a32c4d4
DE-Uehara <r.uehara@defide-ix.com>
2024-06-19 11:29:45 +0900

    両端の空白除去

--- src/app/article/layout.tsx
+++ src/app/article/layout.tsx
@@ -6,7 +6,7 @@ import Footer from "../components/Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="container mx-auto max-w-5xl px-4">
+    <div>
       <Navigation />
       <div className="grid grid-cols-4 gap-4">
         <div className="col-span-3">
