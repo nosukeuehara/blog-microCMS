eb73f7c726c2f2e4f29c335e4fcb635947a891e0
nosukeuehara <ryuunosuke06160616@gamil.com>
2024-06-20 22:16:18 +0900

    フッターのレイアウト修正

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4 border-t border-gray-700 text-sm">
+    <footer className="bg-green-900 text-white py-4 border-t text-sm">
       <div className="container mx-auto text-center">
         <div>&copy; {new Date().getFullYear()} WonderCloud</div>
       </div>
