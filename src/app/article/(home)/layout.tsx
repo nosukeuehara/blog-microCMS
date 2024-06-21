8dbd1190ed9bc7b9c0a6f39cdc5986e70331aab5
nosukeuehara <ryuunosuke06160616@gamil.com>
2024-06-22 01:20:26 +0900

    記事一覧ページレイアウト修正

--- src/app/article/(home)/layout.tsx
+++ src/app/article/(home)/layout.tsx
@@ -1,12 +1,15 @@
-import Categories from "@/app/components/categories";
-import Footer from "@/app/components/footer";
-import HomeIcon from "@/app/components/homeIcon";
+import Categories from "@/app/components/Categories";
+import Footer from "@/app/components/Footer";
+import HomeIcon from "@/app/components/HomeIcon";
+import SearchBar from "@/app/components/SearchBar";
+
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen flex flex-col">
       <HomeIcon />
       <div className="flex-1 flex flex-col">
+        <SearchBar />
         <div className=" text-center">
           <Categories />
         </div>
