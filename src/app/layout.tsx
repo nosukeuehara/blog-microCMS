b3ef71339d7838b47a4488b0f99f5be84eca605c
nosukeuehara <ryuunosuke06160616@gamil.com>
2024-06-22 01:19:38 +0900

    metaData修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "wonder cloud",
+  title: "Wonder Cloud",
 };
 
 export default function RootLayout({
