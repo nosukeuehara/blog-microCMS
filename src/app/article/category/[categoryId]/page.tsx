ceaf5cb18479d59782b9f28dbac3b3ae180d7de4
DE-Uehara <r.uehara@defide-ix.com>
2024-06-24 19:30:01 +0900

    カテゴリ別ページの作成

---
+++ src/app/article/category/[categoryId]/page.tsx
@@ -0,0 +1,46 @@
+import {
+  Blog,
+  MicroCMSResponse,
+  filterCategories,
+  getDetail,
+  getList,
+} from "@/libs/microcms";
+import { notFound } from "next/navigation";
+import ArticleCard from "@/app/components/ArticleCard";
+
+export const revalidate = 10;
+
+export async function generateStaticParams() {
+  const { contents } = await getList<Blog>("blogs");
+
+  const paths = contents.map((post) => {
+    return {
+      postId: post.id,
+    };
+  });
+
+  return [...paths];
+}
+
+export default async function StaticDetailPage({
+  params: { categoryId },
+}: {
+  params: { categoryId: string };
+}) {
+  // const post = await getDetail<Blog>("blogs", categoryName);
+  const contents: Blog[] = await filterCategories(categoryId);
+
+  if (contents.length === 0) {
+    return <div className=" flex justify-center items-center">no articles</div>;
+  } else {
+    return (
+      <div className=" flex flex-col items-center">
+        <ul className=" text-left">
+          {contents.map((content) => (
+            <ArticleCard key={content.id} post={content} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
