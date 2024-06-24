3f1be08239c065f9bb78a68e901ea945373d1e8b
DE-Uehara <r.uehara@defide-ix.com>
2024-06-24 19:29:30 +0900

    記事知覧ページの修正

--- src/app/article/(home)/layout.tsx
+++ src/app/article/(home)/layout.tsx
@@ -1,23 +1,24 @@
+import SearchArticle from "@/app/components/SearchField";
 import Categories from "@/app/components/Categories";
 import Footer from "@/app/components/Footer";
 import HomeIcon from "@/app/components/HomeIcon";
-import SearchBar from "@/app/components/SearchBar";
-
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen flex flex-col">
-      <HomeIcon />
+      <div className="flex justify-between mb-4 border-b border-gray-300 mx-4">
+        <HomeIcon />
+        <SearchArticle />
+      </div>
+      <div className="text-center mb-4">
+        <Categories />
+      </div>
       <div className="flex-1 flex flex-col">
-        <SearchBar />
-        <div className=" text-center">
-          <Categories />
-        </div>
-        <div className="flex-1">
+        <div className="flex-1 mb-8">
           <main>{children}</main>
         </div>
       </div>
-      <div className="">
+      <div className="mt-auto bg-white w-full">
         <Footer />
       </div>
     </div>
--- src/app/article/(home)/page.tsx
+++ src/app/article/(home)/page.tsx
@@ -1,6 +1,5 @@
+import ArticleCard from "@/app/components/ArticleCard";
 import { Blog, getList } from "@/libs/microcms";
-import Image from "next/image";
-import Link from "next/link";
 
 export const revalidate = 10;
 
@@ -14,37 +13,9 @@ export default async function StaticPage() {
   return (
     <div className=" flex flex-col items-center">
       <ul className=" text-left">
-        {contents.map((post) => {
-          return (
-            <li key={post.id} className=" mb-4 mt-4">
-              <Link
-                href={`/article/${post.id}`}
-                className="grid grid-cols-2 gap-4"
-              >
-                <Image
-                  src={post.eyecatch!.url}
-                  alt={"image"}
-                  width={200}
-                  height={200}
-                  className="col-span1"
-                />
-                <div className="">
-                  <div className="">
-                    <div className=" text-lg font-bold">{post.title}</div>
-                    <div className=" text-sm">{`#${post.category}`}</div>
-                    <div>
-                      {Intl.DateTimeFormat("en-US", {
-                        year: "numeric",
-                        month: "long",
-                        day: "numeric",
-                      }).format(new Date(post.createdAt))}
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            </li>
-          );
-        })}
+        {contents.map((post) => (
+          <ArticleCard key={post.id} post={post} />
+        ))}
       </ul>
     </div>
   );
