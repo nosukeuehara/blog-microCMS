a3bf1167f0fae1da3ae36407c573b851ee373b85
DE-Uehara <r.uehara@defide-ix.com>
2024-06-27 14:47:31 +0900

    ホームアイコンのスタイル修正

--- src/app/components/homeIcon.tsx
+++ src/app/components/homeIcon.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import React from "react";
-import Image from "next/image";
 
 interface HomeIconProps {
   src: string;
@@ -8,7 +7,10 @@ interface HomeIconProps {
 
 const HomeIcon = () => {
   return (
-    <Link href="/home" className=" flex font-bold text-lg text-blue-700 p-4">
+    <Link
+      href="/home"
+      className=" flex font-bold text-lg text-blue-600 p-4 transition-colors duration-150 hover:text-blue-400"
+    >
       Wonder Cloud
     </Link>
   );
