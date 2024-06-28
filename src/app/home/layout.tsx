313491d9ed3f38088eecb5f6111ae0b894af7abf
DE-Uehara <r.uehara@defide-ix.com>
2024-06-28 14:39:31 +0900

    usePathnameのビルドエラー修正

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import SearchArticles from "@/app/components/SearchArticles";
 import Footer from "../components/Footer";
 import HomeIcon from "../components/HomeIcon";
+import { Suspense } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -12,7 +13,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       </Header>
       <div className="flex-1 flex flex-col sm:mx-10">
         <div className="flex-1 mb-8">
-          <main>{children}</main>
+          <Suspense>
+            <main>{children}</main>
+          </Suspense>
         </div>
       </div>
       <Footer />
